36bc2099d18b8963786c6453c2ca137a6fa23509
Gard Sandholt <gard.sandholt@gmail.com>
2021-01-19 16:35:16 +0100

    Added vcr cassettes and recorded requests

--- test/yr_forecast_test.rb
+++ test/yr_forecast_test.rb
@@ -1,4 +1,10 @@
 require "test_helper"
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+end
 
 class YrForecastTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -6,22 +12,30 @@ class YrForecastTest < Minitest::Test
   end
 
   def test_get_empty_search
-    forecast = YrForecast.for(place: "")
-    assert_nil forecast
+    VCR.use_cassette("test_get_empty_search") do
+      forecast = YrForecast.for(place: "")
+      assert_nil forecast
+    end
   end
 
   def test_get_data_by_search
-    forecast = YrForecast.for(place: "Bud")
-    assert_equal 14, forecast.count
+    VCR.use_cassette("test_get_data_by_search") do
+      forecast = YrForecast.for(place: "Bud")
+      assert_equal 15, forecast.count
+    end
   end
 
   def test_get_data_by_coords
-    forecast = YrForecast.for(latitude: 59.9128627, longitude: 10.7434443)
-    assert_equal 14, forecast.count
+    VCR.use_cassette("test_get_data_by_coords") do
+      forecast = YrForecast.for(latitude: 59.9128627, longitude: 10.7434443)
+      assert_equal 15, forecast.count
+    end
   end
 
   def test_get_data_by_other_coords
-    forecast = YrForecast.for(latitude: 69.648405, longitude: 18.959708)
-    assert_equal 14, forecast.count
+    VCR.use_cassette("test_get_data_by_other_coords") do
+      forecast = YrForecast.for(latitude: 69.648405, longitude: 18.959708)
+      assert_equal 15, forecast.count
+    end
   end
 end
